01945930e12dd16534beba99ae74470843f48557
Sam Dalton <spd4nf@virginia.edu>
2018-10-30 14:27:09 -0500

    jwt strategy

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,25 @@
 
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+const { JWT_EXPIRY, JWT_SECRET } = require('../config');
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
-router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+function createAuthToken(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user.toJSON());
+  res.json({ authToken });
 });
 
 module.exports = router;
