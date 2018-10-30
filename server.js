67b9557933c8f7e209a3466241557a444af9a571
Sam Dalton <spd4nf@virginia.edu>
2018-10-30 14:29:31 -0500

    jwt strategy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { PORT, MONGODB_URI } = require('./config');
 
 const passport = require('passport');
 const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
 
 const notesRouter = require('./routes/notes');
 const foldersRouter = require('./routes/folders');
